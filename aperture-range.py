f617116b51f93f98f1f3b2dda18584f68be6584f
Carl Ritson <cgr@kent.ac.uk>
2014-05-10 16:14:50 +0100

    Tweak aperture example.

--- aperture-range.py
+++ aperture-range.py
@@ -9,17 +9,22 @@ def camera_main(camera):
     
     camera.set_config('capture', 1)
     camera.set_config('capturetarget', 'Memory card')
+    camera.set_config('reviewtime', 'None')
+    camera.set_config('drivemode', 'Single')
     
     apertures = camera.get_config_choices('aperture')
 
     for aperture in apertures:
         print aperture
+        
         result = camera.set_config('aperture', aperture)
         print 'aperture', result
-        result = camera.set_config('eosremoterelease', 'Press 3')
+        
+        result = camera.set_config('eosremoterelease', 'Press Full')
         print 'press', result
-        time.sleep(0.5)
-        result = camera.set_config('eosremoterelease', 'Release 3')
+        time.sleep(0.3)
+
+        result = camera.set_config('eosremoterelease', 'Release Full')
         print 'release', result
         time.sleep(2.0)
 
